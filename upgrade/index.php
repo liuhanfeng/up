e0b85c214b255d114da4f07c7a0781d8e9f5a1d5
mo <mo@mo.com>
2018-04-19 18:02:59 +0800

    加升级文件.

---
+++ upgrade/index.php
@@ -0,0 +1,23 @@
+<?php
+include_once '../include.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>升</title>
+    <!-- add styles -->
+    <link href="<?php echo $config['sys']['siteurl'] ?>/css/main.css" rel="stylesheet" type="text/css" />
+    <!-- add scripts -->
+    <script src="<?php echo $config['sys']['siteurl'] ?>/js/jquery.min.js"></script>
+
+</head>
+<body>
+<ul>
+    <li><a href="update_img_file_path.php">更新图片地址</a> </li>
+</ul>
+</body>
+</html>
+
+
